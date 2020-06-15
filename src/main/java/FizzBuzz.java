d4e60b7d1ab1a5751edfb98c8298571de80e4547
subrasa <subramaniyan_sudhakar@cat.com>
2020-06-16 00:15:36 +0530

    initial commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,8 @@
+public class FizzBuzz {
+    public String play(int number) {
+        if (number % 3 == 0) return "Fizz";
+        if(number % 5 == 0 ) return "Buzz";
+
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {gt s
+
+    private FizzBuzz fizz;
+    @Before
+    public void setUp(){
+        fizz = new FizzBuzz();
+    }
+
+    @Test
+    public void testNumber() {
+        int number = 1;
+        String returnVal = fizz.play(number);
+        Assert.assertEquals("1", returnVal);
+    }
+
+    @Test
+    public void testFizz(){
+        int number = 3;
+        String returnVal = fizz.play(number);
+        Assert.assertEquals("Fizz", returnVal);
+    }
+
+    @Test
+    public  void testFizzDivisibleby3(){
+        int number = 9;
+        String returnVal = fizz.play(number);
+        Assert.assertEquals("Fizz", returnVal);
+    }
+
+    @Test
+    public void testBuzz(){
+        int number = 5;
+        String returnVal = fizz.play(number);
+        Assert.assertEquals("Buzz", returnVal);
+    }
+
+    @Test
+    public void testBuzzDivisibleby5(){
+        int number = 20;
+        String returnVal = fizz.play(number);
+        Assert.assertEquals("Buzz", returnVal);
+    }
+
+}
